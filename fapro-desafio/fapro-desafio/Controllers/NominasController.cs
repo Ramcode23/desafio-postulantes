a24fff1e0ceb824ae0fd7536591ae7159c1c825e
Raphy Mejia <83966299+Ramcode23@users.noreply.github.com>
2022-04-22 04:26:59 -0700

    Add files via upload
    Esta es mi solucion , en netcore5.

---
+++ fapro-desafio/fapro-desafio/Controllers/NominasController.cs
@@ -0,0 +1,42 @@
+﻿using fapro_desafio.Models;
+using fapro_desafio.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace fapro_desafio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NominasController : ControllerBase
+    {
+        private readonly INominaService service;
+
+        public NominasController(INominaService service)
+        {
+            this.service = service;
+        }
+        // GET: api/<NominasController>
+        [HttpGet]
+        public  IEnumerable<NominaRegistro> Get()
+        {
+
+            try
+            {
+                return  service.GetNominaRegistros();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+
+     
+    }
+}
---
+++ fapro-desafio/fapro-desafio/Models/NominaRegistro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fapro_desafio.Models
+{
+    public class NominaRegistro
+    {
+        public int No { get; set; }
+        public string RazonSocial { get; set; }
+        public string Pais { get; set; }
+        public string DatosInscripcion { get; set; }
+        public string Vigencia { get; set; }
+        public string DatosUltimaActualizacion { get; set; }
+        public string Estado { get; set; }
+    }
+}
